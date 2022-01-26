59a79351fb504775cf7664fc80040d3d3f094347
leotranq <leotranq@gmail.com>
2022-01-26 11:48:56 +0000

    videoSaved

--- src/js/component/HeaderTitle/HeaderTitle.jsx
+++ src/js/component/HeaderTitle/HeaderTitle.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import "../HeaderTitle/Title.css";
+// import BackVideo from "../../../img/backgroundTitle.mp4";
 const HeaderTitle = () => {
 	return (
 		<div>
 			<div className="Container">
 				<div className="Title">
-					<h1>Keyboard Warriors</h1>
-				</div>
+					<h1 className="titleLetters">Keyboard Warriors</h1>
 
-				<div className="Slogan">
-					<h6>
-						Inventamos tu <strong>pasado</strong>, trabajamos tu{" "}
-						<strong>presente</strong> y desarrollammos tu{" "}
-						<strong> Futuro</strong>
-					</h6>
+					<h4>
+						<div className="Slogan"></div>
+						Inventamos tu{" "}
+						<strong className="strongTitle">pasado</strong>,
+						trabajamos tu{" "}
+						<strong className="strongTitle">presente</strong> y
+						desarrollammos tu{" "}
+						<strong className="strongTitle"> Futuro</strong>
+					</h4>
 				</div>
 			</div>
 		</div>
