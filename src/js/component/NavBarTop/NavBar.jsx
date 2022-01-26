16e2d00f1aab7a5af0f5346651c9476a1d08ced7
PedroParraPerez <pedroparra06091994@gmail.com>
2022-01-26 18:47:09 +0000

    update

--- src/js/component/NavBarTop/NavBar.jsx
+++ src/js/component/NavBarTop/NavBar.jsx
@@ -2,53 +2,55 @@ import React from "react";
 import "../NavBarTop/NavBar.css";
 const NavBar = () => {
 	return (
-		<div className="NavBar">
-			<nav className="navbar navbar-expand-lg">
-				<div className="container-fluid">
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#navbarNavDropdown"
-						aria-controls="navbarNavDropdown"
-						aria-expanded="false"
-						aria-label="Toggle navigation">
-						<span className="navbar-toggler-icon"></span>
-					</button>
-					<div
-						className="collapse navbar-collapse"
-						id="navbarNavDropdown">
-						<ul className="navbar-nav">
-							<li className="nav-item">
-								<a
-									className="nav-link"
-									aria-current="page"
-									href="#">
-									Home
-								</a>
-							</li>
-							<li className="nav-item">
-								<a
-									target="_blank"
-									className="nav-link"
-									href="https://github.com/leotranq/Landing-page-react-Keyboard-Warriors/tree/footer">
-									Projects
-								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">
-									Front end
-								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">
-									Back end
-								</a>
-							</li>
-						</ul>
+		<div className="row">
+			<div className="NavBar">
+				<nav className="navbar navbar-expand-lg">
+					<div className="container-fluid">
+						<button
+							className="navbar-toggler"
+							type="button"
+							data-bs-toggle="collapse"
+							data-bs-target="#navbarNavDropdown"
+							aria-controls="navbarNavDropdown"
+							aria-expanded="false"
+							aria-label="Toggle navigation">
+							<span className="navbar-toggler-icon"></span>
+						</button>
+						<div
+							className="collapse navbar-collapse"
+							id="navbarNavDropdown">
+							<ul className="navbar-nav">
+								<li className="nav-item">
+									<a
+										className="nav-link"
+										aria-current="page"
+										href="#">
+										Home
+									</a>
+								</li>
+								<li className="nav-item">
+									<a
+										target="_blank"
+										className="nav-link"
+										href="https://github.com/leotranq/Landing-page-react-Keyboard-Warriors/tree/footer">
+										Projects
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link" href="#">
+										Front end
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link" href="#">
+										Back end
+									</a>
+								</li>
+							</ul>
+						</div>
 					</div>
-				</div>
-			</nav>
+				</nav>
+			</div>
 		</div>
 	);
 };
