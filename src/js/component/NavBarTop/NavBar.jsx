eae05a37557afb4996cc9474def464604882f12f
PedroParraPerez <pedroparra06091994@gmail.com>
2022-01-27 08:26:52 +0000

    finish

--- src/js/component/NavBarTop/NavBar.jsx
+++ src/js/component/NavBarTop/NavBar.jsx
@@ -4,18 +4,18 @@ const NavBar = () => {
 	return (
 		<ul className="nav">
 			<li className="nav-item">
-				<a className="nav-link active" aria-current="page" href="#">
-					<strong>Active</strong>
+				<a className="nav-link active">
+					<strong>Home</strong>
 				</a>
 			</li>
 			<li className="nav-item">
-				<a className="nav-link active" aria-current="page" href="#">
-					<strong>Active</strong>
+				<a className="nav-link active">
+					<strong>Proyects</strong>
 				</a>
 			</li>
 			<li className="nav-item">
-				<a className="nav-link active" aria-current="page" href="#">
-					<strong>Active</strong>
+				<a className="nav-link active">
+					<strong>Front-End</strong>
 				</a>
 			</li>
 		</ul>
